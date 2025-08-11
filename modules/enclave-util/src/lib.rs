5a6318230a4a0ffd24d024ef70d7645f557349e4
yoshidan <naohiro.y@gmail.com>
2025-08-11 19:04:31 +0900

    remove deps

--- modules/enclave-util/src/lib.rs
+++ modules/enclave-util/src/lib.rs
@@ -1,18 +1,20 @@
 #![no_std]
 #[macro_use]
-extern crate sgx_tstd as std;
 extern crate alloc;
 
-use std::ffi::CString;
+use alloc::string::String;
+use alloc::vec::Vec;
 
 extern "C" {
     fn ocall_print(msg: *const i8);
 }
 
 #[no_mangle]
-pub extern "C" fn log(msg: &str) {
-    let s = CString::new(msg).expect("Failed to create CString from message");
+pub extern "C" fn log(msg: String) {
+    let mut buf: Vec<u8> = msg.as_bytes().to_vec();
+    buf.push(0); // NUL終端追加
+
     unsafe {
-        ocall_print(s.as_ptr());
+        ocall_print(buf.as_ptr() as *const i8);
     }
 }
